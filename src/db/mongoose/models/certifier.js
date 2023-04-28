f9aa67be1a1d86387ffab8d1ae3c14ff928d19ff
Otoniel Cantillo <developer@walkerreid.com>
2023-04-28 15:25:06 -0400

    add dc to state list

--- src/db/mongoose/models/certifier.js
+++ src/db/mongoose/models/certifier.js
@@ -25,7 +25,7 @@ const certifierSchema = new mongoose.Schema({
 	licenses: [{
 		state: {
 			type: String,
-			enum: ['Multistate', 'AL', 'MT', 'AK', 'NE', 'AZ', 'NV', 'AR', 'NH', 'CA', 'NJ', 'CO', 'NM', 'CT', 'NY', 'DE', 'NC', 'FL', 'ND', 'GA', 'OH', 'HI', 'OK', 'ID', 'OR', 'IL', 'PA', 'IN', 'RI', 'IA', 'SC', 'KS', 'SD', 'KY', 'TN', 'LA', 'TX', 'ME', 'UT', 'MD', 'VT', 'MA', 'VA', 'MI', 'WA', 'MN', 'WV', 'MS', 'WI', 'MO', 'WY']
+			enum: ['Multistate', 'AL', 'MT', 'AK', 'NE', 'DC', 'AZ', 'NV', 'AR', 'NH', 'CA', 'NJ', 'CO', 'NM', 'CT', 'NY', 'DE', 'NC', 'FL', 'ND', 'GA', 'OH', 'HI', 'OK', 'ID', 'OR', 'IL', 'PA', 'IN', 'RI', 'IA', 'SC', 'KS', 'SD', 'KY', 'TN', 'LA', 'TX', 'ME', 'UT', 'MD', 'VT', 'MA', 'VA', 'MI', 'WA', 'MN', 'WV', 'MS', 'WI', 'MO', 'WY']
 		},
 		number: {
 			type: String
